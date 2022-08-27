a5f9b82c145e264918e2a8e740c47bce1304fd68
Igor Gordeev <gordeev06061987@gmail.com>
2022-08-27 15:49:01 +0300

    modal v2

--- src/js/script.js
+++ src/js/script.js
@@ -6,15 +6,15 @@ $(document).ready(function () {
 		/* adaptiveHeight: true, */  //автовісота картинки
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"</button>',
-		responsive: [
+		/* responsive: [
 			{
 				breakpoint: 992,
 				settings: {
-					dots: true,
+					dots: true,             //адаптация под размер экрана
 					arrows: false
 				}
 			}
-		]
+		] */
 	});
 	//табы(кнопки вібора)
 	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
